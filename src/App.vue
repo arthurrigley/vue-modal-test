18d8ad470f1529e89e9279c21e10bd7adf105a48
Arthur Rigley <hi@arthurrigley.com>
2022-09-29 07:41:36 +0100

    vue modal

---
+++ src/App.vue
@@ -0,0 +1,113 @@
+<template>
+  <button
+    @click="showModal = !showModal"
+  >Show Modal One</button>
+
+  <transition name="overzoom">
+    <ModalOne 
+      modalTitle="Modal One"
+      modalId="modal-one"
+      v-if="showModal"
+      @close-modal="showModal = !showModal"
+    />
+  </transition>
+
+</template>
+
+<script>
+import ModalOne from '@/components/ModalOne.vue'
+
+export default {
+  name: 'App',
+  components: {
+    ModalOne
+  },
+  data() {
+    return {
+      showModal: false
+    }
+  }
+}
+</script>
+
+
+
+
+
+
+
+
+
+<style>
+
+.v-enter-active,
+.v-leave-active {
+  transition: opacity 0.5s ease;
+}
+
+.v-enter-from,
+.v-leave-to {
+  opacity: 0;
+}
+
+* {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+  font-family: 'Rubik', sans-serif;
+}
+
+#app {
+  width: 100vw;
+  height: 100vh;
+  max-width: 600px;
+  padding: 50px;
+}
+
+button {
+  cursor: pointer;
+  display: block;
+  width: 100%;
+  padding: 15px;
+  border-radius: 5px;
+  background-color: rgba(0,150,180,1);
+  border: 8px solid rgba(0,180, 210,0.5);
+  color: white;
+  font-size: 18px;
+  font-weight: 700;
+  transition: all 0.2s;
+  margin: 0 0 30px;
+}
+button:hover {
+  background-color: rgba(0,180, 210,1);
+}
+button:focus {
+  box-shadow: 0 0 0 8px rgba(0,210,240,0.3);
+}
+
+.material-icons {
+  font-family: 'Material Icons';
+  font-weight: normal;
+  font-style: normal;
+  font-size: 24px;  /* Preferred icon size */
+  display: inline-block;
+  line-height: 1;
+  text-transform: none;
+  letter-spacing: normal;
+  word-wrap: normal;
+  white-space: nowrap;
+  direction: ltr;
+
+  /* Support for all WebKit browsers. */
+  -webkit-font-smoothing: antialiased;
+  /* Support for Safari and Chrome. */
+  text-rendering: optimizeLegibility;
+
+  /* Support for Firefox. */
+  -moz-osx-font-smoothing: grayscale;
+
+  /* Support for IE. */
+  font-feature-settings: 'liga';
+}
+
+</style>
---
+++ src/components/ModalOne.vue
@@ -0,0 +1,151 @@
+<template>
+  <div class="modal-bg">
+    <div class="modal">
+      <span
+        @click="closeModal()"
+        class="close-icon material-symbols-outlined">close</span>
+      <h2>{{modalTitle}}</h2>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'ModalOne',
+  props: {
+    modalId: String,
+    modalTitle: String
+  },
+  methods: {
+    closeModal() {
+      this.$emit('close-modal');
+    }
+  }
+}
+</script>
+
+
+
+
+
+<style scoped>
+/* Start During End for in and out */
+/* .v-enter-from {
+  transform: translate(0, 20px) scale(0.9);
+  opacity: 0;
+}
+.v-enter-active {
+  transition: all 0.3s ease-in-out;
+}
+.v-enter-to {
+  transform: translate(0, 0px);
+  opacity: 1;
+}
+.v-leave-from {
+  opacity: 1;
+}
+.v-leave-active {
+  transition: all 0.3s ease-in-out;
+}
+.v-leave-to {
+  opacity: 0;
+  transform: translate(0, 20px) scale(0.9);
+} */
+
+.overzoom-enter-active {
+  animation: vAnimation-in 0.3s;
+}
+
+.overzoom-leave-active {
+  animation: vAnimation-out 1s;
+}
+
+@keyframes vAnimation-in {
+  0% {
+    transform: translate(0, 40px) scale(0.9);
+    opacity: 0;
+  }
+  65% {
+    transform: translate(0, -5px) scale(1.1);
+    opacity: 1;
+  }
+  100% {
+    transform: translate(0, 0) scale(1);
+  }
+}
+
+@keyframes vAnimation-out {
+  0% {
+    transform: translate(0, 0) scale(1);
+    opacity: 1;
+  }
+  30% {
+    opacity: 0;
+  }
+  100% {
+    transform: translate(0, 20px) scale(0);
+    opacity: 0;
+  }
+}
+
+
+
+.modal-bg {
+  position: fixed;
+  top: 0;
+  left: 0;
+  right: 0;
+  bottom: 0;
+  z-index: 100;
+  backdrop-filter: blur(4px);
+}
+
+.modal {
+  position: fixed;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+  display: block;
+  width: 100%;
+  max-width: 400px;
+  background: white;
+  margin: 0 auto;
+  padding: 30px;
+  border-radius: 8px;
+  box-shadow: 0 0 30px -1px rgba(0,0,0,0.5);
+}
+
+.close-icon {
+  position: fixed;
+  display: block;
+  top: -10px;
+  right: -10px;
+  background-color: white;
+  width: 40px;
+  height: 40px;
+  line-height: 40px;
+  border-radius: 100%;
+  text-align: center;
+  box-shadow: 0 0 15px -1px rgba(0,0,0,0.5);
+  cursor: pointer;
+  transition: all 0.3s;
+}
+
+.close-icon:hover {
+  background-color: #f0f0f0;
+  box-shadow: 0 0 20pxpx 0px rgba(0,0,0,0.6);
+}
+
+img {
+  display: inline-block;
+  vertical-align: middle;
+  margin: 0 15px 0 0;
+  width: 100%;
+  max-width: 50px;
+}
+
+h1 {
+  text-align: center;
+}
+
+</style>
